a3b643ded4c07111d6343f86539eb8160941851d
piingy <123731368+piingy@users.noreply.github.com>
2023-08-03 15:50:40 +0200

    Create GeneratePointsOnMesh.h

---
+++ GeneratePointsOnMesh.h
@@ -0,0 +1,37 @@
+// GeneratePointsOnMesh.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GeneratePointsOnMesh.generated.h"
+
+UCLASS()
+class VCC_CARCONFIG01_API AGeneratePointsOnMesh : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AGeneratePointsOnMesh();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation")
+        bool bGeneratePointsInsideHollow;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation")
+        int32 NumberOfPoints;
+
+    UFUNCTION(BlueprintCallable, Category = "Spline")
+        void GeneratePointsInsideHollow();
+
+    UPROPERTY(VisibleAnywhere)
+        class UStaticMeshComponent* MeshComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation")
+        class USplineComponent* SplineComponent;
+protected:
+
+
+    virtual void BeginPlay() override;
+
+    //void GeneratePointsInsideHollow();
+};
